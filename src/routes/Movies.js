887785497794670b7e19f27f819c5ec7ad13958f
jin <kyuj0810@gmail.com>
2023-05-04 15:51:12 +0900

    apolloprovider

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,27 @@
+import { useApolloClient, gql } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
-  return <div></div>
+	const [movies, setMovies] = useState([]);
+	const client = useApolloClient();
+	useEffect(() => {
+		client
+			.query({
+				query: gql`
+					{
+						allMovies {
+							title
+						}
+					}
+				`,
+			})
+			.then((results) => setMovies(results.data.allMovies));
+	}, [client]);
+	return (
+		<div>
+			{movies.map((movie) => (
+				<li key={movie.id}>{movie.title}</li>
+			))}
+		</div>
+	);
 }
